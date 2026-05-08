f3b90fd5549b938611a08bdbdd096d1224a9ce86
naktheking <78290773+naktheking@users.noreply.github.com>
2026-05-07 20:44:15 -0700

    correct version of server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,26 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
-import accountRouter from "./routes/account.js";
+import dotenv from "dotenv";
+import accountRouter from "./route/account.js";
 
 dotenv.config();
-const cors=require("cors");
+
 const app = express();  
 const PORT = 3000;
-
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
 
-
 app.use(express.json());
-
-
-// In-memory user storage
-const users = {};
-
 
 mongoose.connect(process.env.MONGODB_URI)
  .then(() => console.log("Connected to MongoDB"))
  .catch((err) => console.log(err));
 
+app.use(accountRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
