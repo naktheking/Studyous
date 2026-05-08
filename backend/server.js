81c9bcc71f8b58a879faa48b5e31dd2bafc4a4bf
Happy-potamus <zhoularry121@gmail.com>
2026-05-07 20:32:53 -0700

    Revert "mounted routers and deleted unneeded node_modules"
    This reverts commit c0898974a138124d8f2bec40b4a10127730922c3.

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import accountRouter from "./route/account.js";
+import accountRouter from "./routes/account.js";
 
 dotenv.config();
 const cors=require("cors");
 const app = express();  
 const PORT = 3000;
 
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
 
+
 app.use(express.json());
+
+
+// In-memory user storage
+const users = {};
+
 
 mongoose.connect(process.env.MONGODB_URI)
  .then(() => console.log("Connected to MongoDB"))
  .catch((err) => console.log(err));
 
- app.use(accountRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
