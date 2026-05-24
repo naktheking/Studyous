12cf7b365bd132c5d233a91a32ac39670a9b51bb
yuephin879 <yuephin879@gmail.com>
2026-05-24 01:05:26 -0700

    Made post history viewable

--- frontend/src/statistics.jsx
+++ frontend/src/statistics.jsx
@@ -1,16 +1,41 @@
 import { useState, useEffect } from 'react';
 
-function PostStats({ stats, setStats, person })
-{
-    return(
-        <div className="post_info">
-        { stats ? (
-          <p>Stats here</p>
-        ) : (
-          <button className="stats-button" onClick={() => setStats(true)}>View Post History</button>
-        )}
+function PostStats({ stats, setStats, person }) {
+  const [posts, setPosts] = useState('');
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/statistics/get-post/${person}`
+      );
+
+      const postData = await res.json();
+      setPosts(postData);
+      setStats(true);
+    } catch (err) {
+        console.error(err);
+    }
+  };
+
+  return (
+    <div className="post_info">
+    { stats ? (
+      <div>
+        <button type="button" onClick={() => setStats(false)}>Hide Post History</button>
+      {posts.map((p) => (
+        <div key={p._id}>
+          <h4>{p.title}</h4>
+          <p>{p.location}</p>
+          <p>{p.date}</p>
+          <p>{p.startTime} - {p.endTime}</p>
         </div>
-    )
-    
+        ))}
+      </div>
+    ) : (
+      <button className="stats-button" onClick={fetchPosts}>View Post History</button>
+    )}
+    </div>
+  )
+
 }
 export default PostStats;
