95f808f5cc9cbe78aca6a8e6835de73769cc35e6
yuephin879 <yuephin879@gmail.com>
2026-05-26 19:47:02 -0700

    Fix merge conflicts

--- backend/route/post.js
+++ backend/route/post.js
@@ -1,13 +1,25 @@
 import express from "express";
 import Post from "../models/post.js";
+import User_account from "../models/user.js"
 
 const router = express.Router();
 
 router.post("/create-post", async (req, res) => {
   try {
-    console.log("got post");
-    const { title, location, date, startTime, endTime } = req.body;
+    const { person, title, location, date, startTime, endTime } = req.body;
+
+    const user = await User_account.findOne({
+      username: person
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found"
+      });
+    }
+
     const post = await Post.create({ 
+      person: user.username,
       title, 
       location,
       date,
