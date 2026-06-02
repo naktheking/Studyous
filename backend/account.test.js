2e639c5189f94a9f2c43a725a64f3df4d097b8c6
Ansh Aditya Singh <anshadityasingh777@gmail.com>
2026-06-01 18:06:57 -0700

    Create account, login, and remove account tests pass

--- backend/account.test.js
+++ backend/account.test.js
@@ -5,6 +5,7 @@ import app from "./server.js"
 describe("Login Interface", () => {
 
     test("Create account successfully", async () => {
+
         const result = await request(app)
             .post("/account/create-account")
             .send({
@@ -14,6 +15,42 @@ describe("Login Interface", () => {
 
         expect(result.status).toBe(201);
         expect(result.body.username).toBe("Michael");
+
+        const resultRem = await request(app)
+            .post("/account/remove-account")
+            .send({
+                username: "Michael"
+            })
+
+        expect(resultRem.status).toBe(201); 
+    });
+
+    test("Login successfully", async () => {
+        const result = await request(app)
+            .post("/account/create-account")
+            .send({
+                username: "Michael",
+                password: "Jackson"
+            })
+
+        expect(result.status).toBe(201);
+        expect(result.body.username).toBe("Michael");
+
+        const resultlin = await request(app)
+            .get("/account/get-account")
+            .send({
+                username: "Michael"
+            })
+
+        expect(resultlin.status).toBe(200);
+
+         const resultRem = await request(app)
+            .post("/account/remove-account")
+            .send({
+                username: "Michael"
+            })
+
+         expect(resultRem.status).toBe(201);
     });
 
 });
--- backend/route/account.js
+++ backend/route/account.js
@@ -1,5 +1,7 @@
 import express from "express";
 import User_account from "../models/user.js";
+import user from "../models/user.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -7,10 +9,7 @@ router.post("/create-account", async (req, res) => {
   try {
     const { username, password } = req.body;
     
-    console.log("before findOne");
-    console.log(process.env.MONGO_URI);
     const existingAccount = await User_account.findOne({ username });
-    console.log("after findOne");
 
     if (existingAccount) {
       return res.status(409).json({ error: "account already created" });
@@ -37,6 +36,30 @@ router.get("/get-account", async (req, res) => {
     
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/remove-account", async (req, res) => {
+  try {
+    const { username } = req.body;
+    const usern = { username };
+    
+    const existingAccount = await User_account.findOne({ username });
+
+    if (!existingAccount) {
+      return res.status(405).json({ error: "account does not exist" });
+    }
+
+    const deleteAcc = await User_account.deleteOne({ username: usern.username });
+    
+    if (!deleteAcc) {
+      return res.status(407).json({ error: "account not deleted" });
+    }
+
+    return res.status(201).json(existingAccount);
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
   }
 });
 
