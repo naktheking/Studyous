881896b4b7866c41ec87d51644cedcb6b8fbbbd1
Ansh Aditya Singh <anshadityasingh777@gmail.com>
2026-06-03 20:45:51 -0700

    Testing formats for friend features established

--- backend/friend.test.js
+++ backend/friend.test.js
@@ -4,7 +4,7 @@ import app from "./app.js"
 
 describe("Friends Interaction", () => {
 
-    test("Send and Accept Friend Request", async () => {
+    test("Send, Get, and Accept Friend Request", async () => {
 
         const result1 = await request(app)
             .post("/account/create-account")
@@ -34,6 +34,12 @@ describe("Friends Interaction", () => {
             })
 
         expect(resultSendReq.body.success).toBe(true);
+
+        const resultGetReq = await request(app)
+            .get("/friend/get-requests/Jafaar")
+
+        expect(resultGetReq.status).toBe(200);
+        expect(resultGetReq.body.success).toBe(true);
 
         const resultAccReq = await request(app)
             .post("/friend/accept-request")
@@ -113,6 +119,69 @@ describe("Friends Interaction", () => {
             .post("/account/remove-account")
             .send({
                 username: "Tito"
+            })
+
+        expect(resultRem2.status).toBe(201); 
+    });
+
+        test("Get Friends List", async () => {
+
+        const result1 = await request(app)
+            .post("/account/create-account")
+            .send({
+                username: "Small",
+                password: "Smallberg"
+            })
+
+        expect(result1.status).toBe(201);
+        expect(result1.body.username).toBe("Small");
+
+        const result2 = await request(app)
+            .post("/account/create-account")
+            .send({
+                username: "Medium",
+                password: "Smallberg"
+            })
+
+        expect(result2.status).toBe(201);
+        expect(result2.body.username).toBe("Medium");
+
+        const resultSendReq = await request(app)
+            .post("/friend/send-request")
+            .send({
+                fromUsername: "Small",
+                toUsername: "Medium"
+            })
+
+        expect(resultSendReq.body.success).toBe(true);
+
+        const resultAccReq = await request(app)
+            .post("/friend/accept-request")
+            .send({
+                fromUsername: "Small",
+                toUsername: "Medium"
+            })
+
+        expect(resultAccReq.body.success).toBe(true);
+
+        const resultGetF = await request(app)
+            .get("/friend/get-friends/Medium")
+
+        expect(resultGetF.status).toBe(200);
+        expect(resultGetF.body.success).toBe(true);
+
+        const resultRem1 = await request(app)
+            .post("/account/remove-account")
+            .send({
+                username: "Small"
+            })
+
+        expect(resultRem1.status).toBe(201); 
+
+        const resultRem2 = await request(app)
+            .post("/account/remove-account")
+            .send({
+                username: "Medium"
             })
 
         expect(resultRem2.status).toBe(201); 
